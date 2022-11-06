4587e312b4530ad76a85b0f8e9245e2b7cd4ceb5
joreims <jorge.garcia14624@est.uca.edu.ni>
2022-11-06 00:59:29 -0600

    PORFIN TERMINE ESTO

---
+++ SwingChallenge/src/swingchallenge/main.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package swingchallenge;
+
+import formulario.forms;
+import javax.swing.JFrame;
+
+
+/**
+ *
+ * @author jegar
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame forms = new forms();
+        forms.setVisible(true);
+        forms.setLocationRelativeTo(null);
+    }
+    
+}
